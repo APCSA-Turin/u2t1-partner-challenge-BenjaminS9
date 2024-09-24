84eb2f8653d38e9a02d4ccfa21ac655484900bf6
BenjaminS9 <benjamins235@nycstudents.net>
2024-09-24 12:43:45 +0000

    commit

--- src/main/java/com/example/project/CalculatorRunner.java
+++ src/main/java/com/example/project/CalculatorRunner.java
@@ -2,11 +2,8 @@ package com.example.project;
 
 public class CalculatorRunner {
     public static void main(String[] args) {
+        Calculator calc = new Calculator("TI-84");
 
-        //test your program here
-        //1. create an instance of the calculator class
-
-        //2. call any methods of that class to test
-
+        
     }
 }
